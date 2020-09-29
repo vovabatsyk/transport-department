0c79d7ae3796f60b003f6709ade7ba98b6d878ac
Volodymyr Batsyk <vovabatsyk@gmail.com>
2020-09-30 01:43:03 +0300

    Added Modal

--- js/init.js
+++ js/init.js
@@ -4,9 +4,12 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
+    $('.modal').modal();
+    $('.slider').slider();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
 
 
 
