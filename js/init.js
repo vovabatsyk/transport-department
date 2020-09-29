e0d488962688c091654f303361187e9f3fc56810
Volodymyr Batsyk <vovabatsyk@gmail.com>
2020-09-30 00:47:34 +0300

    Added Sections: Info, Contacts

--- js/init.js
+++ js/init.js
@@ -3,6 +3,12 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+
+
+
